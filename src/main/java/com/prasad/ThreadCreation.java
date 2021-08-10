e813448591460858136933644ddb23a308ae14c9
prasad 2 patil <prasad.2.patil@atos.net>
2021-08-10 06:31:03 +0530

    thread creation and Volitile keyword

---
+++ src/main/java/com/prasad/ThreadCreation.java
@@ -0,0 +1,72 @@
+package com.prasad;
+
+/**
+ * Hello world!
+ *
+ */
+public class ThreadCreation 
+{
+    public static void main( String[] args )
+    {
+        // Extending Thread class
+        System.out.println("Threads created using extending Thread class");
+        Runner runner1 = new Runner();
+        Runner runner2 = new Runner();
+        runner1.start();        
+        runner2.start();
+
+        // Implementing Runnable interface
+        System.out.println("Threads created using Implementing Runnable interface");
+        Thread runner3 = new Thread(new Runner1());
+        Thread runner4 = new Thread(new Runner1());
+        runner3.start();       
+        runner4.start();
+
+        //Annonymus class
+        System.out.println("Threads created using Annonymus class");
+        Thread runner = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                for(int i=0; i<10; i++){
+                    System.out.println("Hello " + i);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        runner.start();
+    }
+}
+
+// Extending Thread class
+class Runner extends Thread{
+    @Override
+    public void run() {
+        for(int i=0; i<10; i++){
+            System.out.println("Hello " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+// Implementing Runnable interface
+class Runner1 implements Runnable{
+    @Override
+    public void run() {
+        for(int i=0; i<10; i++){
+            System.out.println("Hello " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/prasad/VolatileDemo.java
@@ -0,0 +1,37 @@
+package com.prasad;
+
+import java.util.Scanner;
+
+class Processor extends Thread{
+    private volatile boolean running = true;
+
+    @Override
+    public void run() {
+        while(running){
+            System.out.println("Hello ");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void shutdown() {
+        this.running = false;
+    }
+}
+
+public class VolatileDemo {
+    public static void main(String[] args) {
+        Processor p = new Processor();
+        p.start();
+
+        System.out.println("Enter Return to stop...");
+        Scanner scan = new Scanner(System.in);
+        scan.nextLine();
+
+        p.shutdown();
+        scan.close();
+    }
+}
